ca75cdfcfd32ebd595c851d6ebe827f447d0e8d4
ldcas-uc3m <78721925+ldcas-uc3m@users.noreply.github.com>
2023-02-02 20:56:18 +0100

    zip script + sum touches

---
+++ zip.sh
@@ -0,0 +1,23 @@
+# Bash script to zip the whole project in
+# order to make it deriverable
+
+# please make sure zip and texlive are installed
+
+
+# make sure git is up to date
+git pull
+git checkout main
+
+# compile the report (and save it to root folder)
+cd report
+cp report.tex tmp.tex
+pdflatex tmp.tex -output-directory .. -halt-on-error
+rm tmp.tex
+cd ..
+mv tmp.pdf report.pdf
+rm tmp.*
+
+# zip it (excluding useless stuff)
+zip -r outfile.zip . -x zip.sh report\* .git\* README.md
+
+
