3642edc92775371f834aef2ab5c8c7372ccc2426
ldcas-uc3m <100429021@alumnos.uc3m.es>
2023-05-15 13:31:49 +0200

    fix zip.sh

--- zip.sh
+++ zip.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # Bash script to zip the whole project in order to make it deriverable
-# please make sure zip and texlive are installed
+# please make sure zip, pv and texlive are installed
 
+OUTFILE=../outfile.zip
 
 # make sure git is up to date
+echo "Making sure git is up to date..."
 git pull
 git checkout main
 
 # compile the report (and save it to root folder)
+echo "Compiling the report..."
 cd report
 
-cp report.tex tmp.tex
-pdflatex tmp.tex -output-directory .. -halt-on-error
-rm tmp.tex
+pdflatex report.tex -halt-on-error > /dev/null
+cp report.pdf ..
 
 cd ..
-mv tmp.pdf report.pdf
-rm tmp.*
 
 # remove whatever is in src/.gitignore
+echo "Removing files in .gitignore..."
 cd src
 
-# TODO: test this shit
 for f in $(cat .gitignore) ; do 
     rm -rf "$f"
 done
@@ -29,7 +29,9 @@ done
 cd ..
 
 # zip it (excluding useless stuff)
-zip -r ../outfile.zip . -x zip.sh report\* *.git\* README.md
+echo "Zipping..."
+zip -r $OUTFILE . -x zip.sh report/\* \*.git\* README.md
 
 # cleanup
+echo "Cleaning up..."
 git reset --hard HEAD
