5eca243075166b7c532a6b3fa7305963d534db3a
Luis Daniel Casais Mezquida <78721925+ldcas-uc3m@users.noreply.github.com>
2024-04-06 13:30:18 +0200

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -2,14 +2,16 @@
 # Bash script to zip the whole project in order to make it deriverable
 # please make sure zip and texlive are installed
 
+set -e  # exit on error
+
 OUTFILE=../outfile.zip
 [ -e $OUTFILE ] && rm $OUTFILE  # remove if exists already
+
 
 # compile the report (and save it to root folder)
 echo "Compiling the report..."
 
-latexmk -cd -shell-escape -pdf report/report.tex 
-
+latexmk -cd -shell-escape -silent -pdf report/report.tex 
 cp report/report.pdf .
 
 
@@ -21,7 +23,7 @@ cd ..
 
 # zip it (excluding useless stuff)
 echo "Zipping..."
-zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* README.md
+zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* *__pycache__/\* .venv/\* build/\* .vscode/\*
 
 # cleanup
 echo "Cleaning up..."
